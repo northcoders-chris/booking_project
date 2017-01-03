8ca8dbf4e96ce8b685ac1412695b6f298b83c078
Chris Hill <chris@northcoders.com>
2017-01-03 15:50:21 +0000

    Add guys

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Welcome to the booking app!')
+  res.send('Welcome to the booking app people!')
 })
 
 app.listen(3000, function () {
