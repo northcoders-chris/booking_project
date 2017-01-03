1a5410ee20d26ba4e99175d20654f48609ca42af
Chris Hill <chris@northcoders.com>
2017-01-03 15:57:38 +0000

    [FEAT-3] Added sellers route

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
   res.send('Welcome to the booking app people!')
 })
 
+app.get('/sell', function (req, res) {
+  res.send('Welcome to the sellers area')
+})
+
 app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!')
 })
